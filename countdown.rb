19508659a7e068285ea1d2c20857c9ad997c05b6
Kristen Robertson <kristen77@gmail.com>
2018-09-22 15:03:06 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end
+
+puts "HAPPY NEW YEAR!"
+
+def countdown_with_sleep
+  countdown
+  sleep 5
 end
